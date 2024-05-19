218630eb727fe9e3eed6c2c732417ee3b04c211d
arianpucheta <ariangomez2005@gmail.com>
2024-05-18 22:35:05 -0300

    MODIFICACION DE HTML,CSS,JS, preparacion para agregar los productos

--- CrowAi/activos/js/js-pag-principal/script.js
+++ CrowAi/activos/js/js-pag-principal/script.js
@@ -28,45 +28,10 @@ window.onscroll = function() {
 
 
 
-// -------------Informacion de servicios------------
+// -------------Informacion de productos------------
 
-// let mostrador = document.getElementById("mostrador");
-// let seleccion = document.getElementById("seleccion");
-// let imgSeleccionada = document.getElementById("img");
-// let modeloSeleccionado = document.getElementById("modelo");
-// let descripSeleccionada = document.getElementById("descripcion");
-// let spanInfSeleccionado = document.getElementById("span-inf");
 
-// function cargar(item){
-// 	quitarBordes();
-// 	mostrador.style.width = "60%";
-// 	seleccion.style.width = "40%";
-// 	seleccion.style.opacity = "1";
-// 	seleccion.style.border = "2px solid #00155a"; 
-// 	item.style.border = "2px solid #00155a";
-
-// 	imgSeleccionada.src = item.getElementsByTagName("img")[0].src;
-
-// 	modeloSeleccionado.innerHTML =	item.getElementsByTagName("p")[0].innerHTML;
-
-// 	descripSeleccionada.innerHTML = "Descripcion del modelo ";
-
-// 	spanInfSeleccionado.innerHTML =	item.getElementsByTagName("span")[0].innerHTML;
-
-// }
-
-// function cerrar(){
-//     mostrador.style.width = "100%";
-//     seleccion.style.width = "0%";
-//     seleccion.style.opacity = "0";
-//     quitarBordes();
-// }
-// function quitarBordes(){
-//     var items = document.getElementsByClassName("item");
-//     for(i=0;i <items.length; i++){
-//         items[i].style.border = "";
-//     }
-// }
+// PONER FUNCIONES DE MOSTRAR PRODUCTOS
 
 
 //----------------- FUNCION DE BUSQUEDA --------------------------
