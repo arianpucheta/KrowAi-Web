8d4b9dccd16a474c5e3c3516d4c9866a3b012cc7
arianpucheta <ariangomez2005@gmail.com>
2024-08-07 15:30:48 -0300

    activacion del cache

--- worker.js
+++ worker.js
@@ -1,48 +1,48 @@
-// const version = "version 1";
+const version = "version 1";
 
-// // Instalación del Service Worker
-// self.addEventListener("install", e => {
-//     console.log("Instalando Service Worker");
-//     e.waitUntil(
-//         caches.open(version).then(cache => {
-//             return cache.addAll(["index.html"]).then(() => {
-//                 console.log("Información cacheada");
-//             }).catch(err => {
-//                 console.log(err);
-//             });
-//         })
-//     );
-// });
+// Instalación del Service Worker
+self.addEventListener("install", e => {
+    console.log("Instalando Service Worker");
+    e.waitUntil(
+        caches.open(version).then(cache => {
+            return cache.addAll(["index.html"]).then(() => {
+                console.log("Información cacheada");
+            }).catch(err => {
+                console.log(err);
+            });
+        })
+    );
+});
 
-// // Activación del Service Worker
-// self.addEventListener("activate", e => {
-//     e.waitUntil(
-//         caches.keys().then(keys => {
-//             return Promise.all(
-//                 keys.map(key => {
-//                     if (key !== version) {
-//                         console.log("Cache antiguo eliminado");
-//                         return caches.delete(key);
-//                     }
-//                 })
-//             );
-//         })
-//     );
-// });
+// Activación del Service Worker
+self.addEventListener("activate", e => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if (key !== version) {
+                        console.log("Cache antiguo eliminado");
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
 
-// // Intercepción de solicitudes de red
-// self.addEventListener("fetch", e => {
-//     e.respondWith(
-//         caches.match(e.request).then(respuestaEnCache => {
-//             if (respuestaEnCache) {
-//                 return respuestaEnCache;
-//             }
-//             return fetch(e.request).then(response => {
-//                 return caches.open(version).then(cache => {
-//                     cache.put(e.request, response.clone());
-//                     return response;
-//                 });
-//             });
-//         })
-//     );
-// });
+// Intercepción de solicitudes de red
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(respuestaEnCache => {
+            if (respuestaEnCache) {
+                return respuestaEnCache;
+            }
+            return fetch(e.request).then(response => {
+                return caches.open(version).then(cache => {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
